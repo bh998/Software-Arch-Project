92624a9e3deede061104dc20a98bcaa82a56cff5
bh998 <bh998@msstate.edu>
2016-12-07 02:08:48 -0600

    Started working on database utiility class

---
+++ PHP/Classes/DB_Util.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database{
+    
+    var $host = "localhost";
+    var $user = "root";
+    var $pass = "ab1234";
+    var $db = "myDB";
+    
+    function open_DB(){
+        
+    }
+    
+    function send_query(){
+        
+    }
+    
+}
+
+?>
