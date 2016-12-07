186f32874cdce17b15256cbd0801f378c61c0287
bh998 <bh998@msstate.edu>
2016-12-07 14:35:07 -0600

    Moved the html files in with the php and started account page

--- PHP/Views/homepage.php
+++ PHP/Views/homepage.php
@@ -5,39 +5,46 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+
+<link rel="stylesheet" href="../../Libraries/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+<script src="../../Libraries/jquery-3.1.1.js"></script>
+<script src="../../Libraries/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+</head>
 
 <body>
-
-<?php
-if(is_null($_SESSION["username"])){
-    echo "<h4>I'm sorry you are not allowed to access this page.</h4>";
-    echo "<a href='../../index.html'>Please Login.</a>";
-    exit();
-}
-echo "<h3>Welcome " . $_SESSION["username"] . "!</h3>";
-?>
-<p>Choose a link to begin.</p>
+<div class="container-fluid">
+  <div class="row">
+    <h3 class="bg-info text-center">Welcome To Tradier Stock System</h3>
+  </div>
+  <div class="row">
+  <div class="btn-group btn-group-justified">
+    <a href="homepage.php" class="btn btn-primary">Home</a>
+    <a href="account.php" class="btn btn-primary">Account</a>
+  </div>
+  </div>
+</div>
 
 <br>
 <button type="button" class="btn btn-default">
 <span class="glyphicon glyphicon-hand-up"></span>
-<a href="../../HTML/Views/financial_api.html">Financial News!</a>
+<a href="financial_api.html">Financial News!</a>
 </button><br><br>
 
 <button type="button" class="btn btn-default">
 <span class="glyphicon glyphicon-hand-up"></span>
-<a href="../../HTML/Views/email_validator.html">Email Validator!</a>
+<a href="email_validator.html">Email Validator!</a>
 </button><br><br>
 
 <button type="button" class="btn btn-default">
 <span class="glyphicon glyphicon-hand-up"></span>
-<a href="../../HTML/Views/tradier_api.html">Tradier Info!</a>
+<a href="tradier_api.html">Tradier Info!</a>
 </button><br><br>
 
 <button type="button" class="btn btn-default">
 <span class="glyphicon glyphicon-hand-up"></span>
-<a href="../../PHP/Views/twitter_api.php">Twitter Search!</a>
+<a href="twitter_api.php">Twitter Search!</a>
 </button><br><br>
 
 </body>
